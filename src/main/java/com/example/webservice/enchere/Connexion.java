6ba0f73590e89cd761de058a957e39e500a460b9
Hasinafitiavna <99530058+Hasinafitiavna@users.noreply.github.com>
2023-02-03 10:00:03 +0300

    farany
    farany

--- src/main/java/com/example/webservice/enchere/Connexion.java
+++ src/main/java/com/example/webservice/enchere/Connexion.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 
 public class Connexion {
     public static Connection getConn() throws Exception{
-        String url="jdbc:postgresql://localhost:5432/enchere";
+        String url="jdbc:postgresql://containers-us-west-150.railway.app:8041/railway";
         String user="postgres";
-        String pass="hasina";
+        String pass="MRSBzgoeDvZgCCqSqEE4";
         Class.forName("org.postgresql.Driver");
         Connection conn = DriverManager.getConnection(url,user,pass);
         return conn;
